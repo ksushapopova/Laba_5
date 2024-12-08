89b364c114f3c13bc098992190c1e600e4bbb68d
ksushapopova <ks.popova.2005@mail.ru>
2024-12-08 23:39:05 +0300

    Tests

--- src/test/java/org/example/InjectorTest.java
+++ src/test/java/org/example/InjectorTest.java
@@ -20,34 +20,27 @@ public class InjectorTest {
 
     @Test
     public void testInject() throws Exception {
-        // Создаем экземпляр SomeBean
         SomeBean someBean = new SomeBean();
 
-        // Выполняем инъекцию зависимостей
         injector.inject(someBean);
 
-        // Проверяем, что зависимости инициализированы
         assertNotNull(someBean.getField1(), "field1 should be injected");
         assertNotNull(someBean.getField2(), "field2 should be injected");
     }
 
     @Test
     public void testFoo() throws Exception {
-        // Перенаправляем вывод для проверки
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(outputStream);
         System.setOut(printStream);
 
-        // Создаем экземпляр SomeBean
         SomeBean someBean = new SomeBean();
 
-        // Выполняем инъекцию зависимостей
         injector.inject(someBean);
 
-        // Вызываем метод foo
+
         someBean.foo();
 
-        // Проверяем вывод
         String output = outputStream.toString();
         assertTrue(output.equals("BC") || output.equals("CB"), "Output should be 'BC' or 'CB'");
     }
